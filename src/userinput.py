1700db12f1ec0e4025873ea704c14c128cdd8b37
Sandeep Karem <sandeepkarem.online@gmail.com>
2024-10-13 23:15:39 +0530

    updated

--- src/userinput.py
+++ src/userinput.py
@@ -1,21 +1,2 @@
-# Refer README file for detailed explanation.
-
-name = "Sandeep"
-print(name) #output: Sandeep
-print(type(name))   #output: str (String), type() is used to get the type of the variable
-print(id(name)) # id() is used to get the address.
-
-age = 19
-print(age)  #output: 19
-print(type(age))    #output: int
-print(id(age))  #Address
-
-balance = 7.5
-print(balance)  #output: 7.500000
-print(type(balance))    #float
-print(id(balance))
-
-
-greet = input("enter name: ")
-print("Hi "+ greet)
-
+name = input("enter name: ")
+print(f'hey {name}!')
