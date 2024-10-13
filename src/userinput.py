5f557de9c7347cf65be9ac2827e90010ae38e3a2
Sandeep Karem <sandeepkarem.online@gmail.com>
2024-10-13 18:33:18 +0530

    Update userinput.py

--- src/userinput.py
+++ src/userinput.py
@@ -17,5 +17,8 @@ print(id(balance))
 
 
 greet = input("enter name: ")
-print("Hi "+ greet)
+print("Hi "+ greet)  #output: Hi Sandeep
+
+#in this code we are adding two strings which is nothing but String concatination.
+# Note: we can concatinate only when the data is uniform, which means, if we consider two strings it's possible to concatinate them.But it not possible in the case of adding an integer and a string
 
